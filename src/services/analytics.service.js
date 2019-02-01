5710e74f811dc033bf008510b1c5097511508f1a
Daniel Stewart <daniel.robbins.stewart@gmail.com>
2019-01-31 22:46:30 -0700

    (fix): Socket errors

--- src/services/analytics.service.js
+++ src/services/analytics.service.js
@@ -3,9 +3,7 @@ import * as dotenv from 'dotenv'
 
 dotenv.config()
 
-const baseUrl = process.env.NODE_ENV === 'DEVELOPEMNT' ? 'http://localhost:8080' : 'https://twang-api.herokuapp.com'
-
-axios.defaults.baseURL = baseUrl()
+axios.defaults.baseURL = process.env.NODE_ENV === 'DEVELOPEMNT' ? 'http://localhost:8080' : 'https://twang-api.herokuapp.com'
 
 export const querySentimentData = async (keyword) => {
   const response = await axios.get('/twitter/open-stream', {
