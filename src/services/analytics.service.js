afeb6d5f16a2de53ea9fae7c61af0004ad45708c
Daniel Stewart <daniel.robbins.stewart@gmail.com>
2019-01-31 22:02:01 -0700

    (chore): Update axios baseUrl

--- src/services/analytics.service.js
+++ src/services/analytics.service.js
@@ -1,6 +1,20 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:8080'
+axios.defaults.baseURL = getApiUrl()
+
+const getApiUrl = () => {
+  switch(process.env.NODE_ENV) {
+    case 'DEVELOPMENT':
+      console.log('DEVELOPMENT')
+      return 'http://localhost:8080'
+    case 'PRODUCTION':
+      console.log('PRODUCTION')
+      return 'http://twang-api.herokuapp.com'
+    default: 
+      'http://localhost:8080'
+    break;
+  }
+}
 
 export const querySentimentData = async (keyword) => {
   const response = await axios.get('/twitter/open-stream', {
